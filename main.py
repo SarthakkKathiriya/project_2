fdccca0c4ac7fb7573e600c5ed0b337ac6af17c4
SarthakkKathiriya <skkathiriya012345@gmail.com>
2023-08-03 04:00:19 +0530

    code is done and we are getting required output

--- main.py
+++ main.py
@@ -1,21 +1,22 @@
 
 from tkinter import *
 from tkinter.ttk import *
-from tkcalendar import *
-# writing code needs to
-# create the main window of
-# the application creating
-# main window object named root
+from tkcalendar import Calendar
+
+def your_date():
+    date.config(text="Selected Date: " + my_cal.get_date())
+    # 
 root = Tk()
- 
-# giving title to the main window
-root.title("First_Program")
- 
-# Label is what output will be
-# show on the window
-label = Label(root, text ="Hello World !").pack()
- 
-# calling mainloop method which is used
-# when your application is ready to run
-# and it tells the code to keep displaying
+root.title('my calendar')
+root.geometry('600x500')
+
+my_cal = Calendar(root, selectmode="day", year=2023, month=8, day=2)
+my_cal.pack(pady=10)
+
+date_btn=Button(root, background="blue", text='Select the Date',command=your_date)
+date_btn.pack(pady=5)
+
+date = Label(root, text = "")
+date.pack(pady = 5)
+
 root.mainloop()
