b952a4390ccdbdee9c817c37d96ef7a44f1cc0c2
SarthakkKathiriya <skkathiriya012345@gmail.com>
2023-08-03 04:01:18 +0530

    code is done and we are getting required output

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from tkcalendar import Calendar
 
 def your_date():
     date.config(text="Selected Date: " + my_cal.get_date())
-    # 
+    # question for why bg and fg which are in main documentation not working hear.
 root = Tk()
 root.title('my calendar')
 root.geometry('600x500')
@@ -13,7 +13,7 @@ root.geometry('600x500')
 my_cal = Calendar(root, selectmode="day", year=2023, month=8, day=2)
 my_cal.pack(pady=10)
 
-date_btn=Button(root, background="blue", text='Select the Date',command=your_date)
+date_btn=Button(root,  text='Select the Date',command=your_date)
 date_btn.pack(pady=5)
 
 date = Label(root, text = "")
